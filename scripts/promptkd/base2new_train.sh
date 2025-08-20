4e86451d9e32d69338631c728e34eb3a76bcaebb
秦勇（qinyong） <60870424+qinyong123456@users.noreply.github.com>
2025-08-20 11:06:08 +0800

    Update base2new_train.sh

--- scripts/promptkd/base2new_train.sh
+++ scripts/promptkd/base2new_train.sh
@@ -4,7 +4,7 @@
 DATA="/kaggle/working/PromptKDLOGIT/datasets/data"
 TRAINER=PromptKD
 
-DATASET=$1 # 'imagenet' 'caltech101' 'dtd' 'eurosat' 'fgvc_aircraft' 'oxford_flowers' 'food101' 'oxford_pets' 'stanford_cars' 'sun397' 'ucf101'
+DATASET=$1  # 'imagenet' 'caltech101' 'dtd' 'eurosat' 'fgvc_aircraft' 'oxford_flowers' 'food101' 'oxford_pets' 'stanford_cars' 'sun397' 'ucf101'
 SEED=$2
 
 CFG=vit_b16_c2_ep20_batch8_4+4ctx
@@ -12,8 +12,17 @@ SHOTS=0
 
 DIR=output/base2new/train_base/${DATASET}/shots_${SHOTS}/${TRAINER}/${CFG}/seed_${SEED}
 
-# fgvc_aircraft, oxford_flowers, dtd: KD_WEIGHT:200
-# imagenet, caltech101, eurosat, food101, oxford_pets, stanford_cars, sun397, ucf101, KD_WEIGHT:1000
+# 根据数据集自动设置KD_WEIGHT
+case $DATASET in
+    fgvc_aircraft|oxford_flowers|dtd)
+        KD_WEIGHT=200.0
+        ;;
+    *)
+        KD_WEIGHT=1000.0  # 其他数据集使用1000.0
+        ;;
+esac
+
+echo "使用数据集: $DATASET，设置KD_WEIGHT为: $KD_WEIGHT"
 
 CUDA_VISIBLE_DEVICES=0 python train.py \
     --root ${DATA} \
@@ -25,7 +34,7 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
     DATASET.NUM_SHOTS ${SHOTS} \
     TRAINER.MODAL base2novel \
     TRAINER.PROMPTKD.TEMPERATURE 1.0 \
-    TRAINER.PROMPTKD.KD_WEIGHT 1000.0 
-    
-    set TRAINER.PROMPTKD.LOGIT_STANDARDIZATION=True \
+    TRAINER.PROMPTKD.KD_WEIGHT ${KD_WEIGHT} \
+    TRAINER.PROMPTKD.LOGIT_STANDARDIZATION=True \
     TRAINER.PROMPTKD.ADAPTIVE_TEMPERATURE True
+    
